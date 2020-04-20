0df447846b079640a3c32130a68f759d73697240
yqlbu <31861128+yqlbu@users.noreply.github.com>
2020-04-21 01:54:04 +0800

    done

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd ~
+wget https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh
+sudo sh Archiconda3-0.2.3-Linux-aarch64.sh
+rm -rf Archiconda3-0.2.3-Linux-aarch64.sh
+cd ~
+sudo chown -R $USER archiconda3/
+export PATH=~/archiconda3/bin:$PATH
+conda config --add channels conda-forge
+conda -V
+echo -e " ->  Congratuallations! Archiconda has been installed successfully!"
+}
